ea1959e5a8b5d8a41c1de305c31e744d3a449abb
asad-khilji <khiljiasad2@gmail.com>
2022-03-21 13:36:37 -0500

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// numbers array
+var numbersChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//lowercase array
+var lowerCaseChar = [
+  "a",  "b",  "c", "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z"
+];
+//uppcase array
+var upperCaseChar = [
+  "A",  "B",  "C",  "D",  "E",  "F",  "G",  "H",  "I",  "J",  "K",  "L",  "M",  "N",  "O",  "P",  "Q",  "R",  "S",  "T",  "U",  "V",  "W",  "X",  "Y",  "Z"
+];
+// special char array
+var specialChar = [
+  "@",  "%",  "+",  "\\",  "/",  "'",  "!",  "#",  "$",  "^",  "?",  ":",  ",",  ")",  "(",  "}",  "{",  "]",  "[",  "~",  "-",  "_",  "."
+];
+
 // Assignment code here
 function generatePasswordInfo() {
   var getPasswordLength = parseInt(prompt("Choose number of characters for your password."));
@@ -88,11 +103,11 @@ function generatePassword() {
   }
 
   // Iterate through the password length, get random index from the array of available characters
-  for (var i = 0; i < passwordIncludes.length; i++) {
-    availableCharacters = getRandomChar(availableCharacters);
+  for (var i = 0; i < passwordIncludes.passwordLength; i++) {
+    var c = getRandomChar(availableCharacters);
 
     // add characters to password array
-    createdPassword.push(availableCharacters);
+    createdPassword.push(c);
   }
   
   return createdPassword.join("");
@@ -109,7 +124,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.innerHTML = password;
 
 }
 
